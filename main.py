724758df32919cdf2b092d9c31ad9a427d975545
J-BONNEY <bonfacemutui44@gmail.com>
2025-10-23 21:07:24 +0300

    my flask study

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+# products route
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+#sales route
+@app.route('/sales')
+def sales():
+    return render_template('sales.html')
+#stock route
+@app.route('/stock')
+def stock():
+    return render_template('stock.html')
+app.run()

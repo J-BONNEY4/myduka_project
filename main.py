7ca849d558c16c7f750322a8c377a399b27a56c9
J-BONNEY <bonfacemutui44@gmail.com>
2025-10-30 06:53:39 +0300

    flask day 5

--- main.py
+++ main.py
@@ -1,59 +1,64 @@
-from flask import Flask,render_template,request,redirect,url_for
-from database import fetch_data,insert_products,insert_sales,insert_stock
-app=Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
+from database import fetch_data, insert_products, insert_sales, insert_stock
+app = Flask(__name__)
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 # products route
+
+
 @app.route('/products')
 def products():
-    prods=fetch_data('products')
+    prods = fetch_data('products')
     # print(prods)
-    return render_template('products.html',my_products=prods)
+    return render_template('products.html', my_products=prods)
 
-#add products.
-@app.route('/add_products',methods=['GET','POST'])
+# add products.
+
+
+@app.route('/add_products', methods=['GET', 'POST'])
 def add_products():
-    if request.method=='POST':
-        pname=request.form['name']
-        sp=request.form['Selling_Price']
-        bp=request.form['Buying_Price']
-        new_products=(pname,bp,sp)
+    if request.method == 'POST':
+        pname = request.form['name']
+        sp = request.form['Selling_Price']
+        bp = request.form['Buying_Price']
+        new_products = (pname, bp, sp)
         insert_products(new_products)
         return redirect(url_for('products'))
     return redirect(url_for('products'))
 
 
-
-#sales route
+# sales route
 @app.route('/sales')
 def sales():
-    products=fetch_data('products')
-    Mys=fetch_data('sales')
+    products = fetch_data('products')
+    Mys = fetch_data('sales')
     # print(Mys)
-    return render_template('sales.html',my_sales=Mys,products=products)
-#add sales
+    return render_template('sales.html', my_sales=Mys, products=products)
+# add sales
 
-@app.route('/add_sales',methods=['GET','POST'])
+
+@app.route('/add_sales', methods=['GET', 'POST'])
 def add_sales():
-    if request.method=='POST':
-        prod_id=request.form['pid']
-        pq=request.form['Pquantity']
-        new_sales=(prod_id,pq)
+    if request.method == 'POST':
+        prod_id = request.form['pid']
+        pq = request.form['Pquantity']
+        new_sales = (prod_id, pq)
         insert_sales(new_sales)
         return redirect(url_for('sales'))
     return redirect(url_for('sales'))
 
 
-
-
-#stock route
+# stock route
 @app.route('/stock')
 def stock():
+    products = fetch_data('products')
     Mysto=fetch_data('stock')
     # print(Mysto)
-    return render_template('stock.html',my_stock=Mysto)
+    return render_template('stock.html',my_stock=Mysto,products=products)
  #add stock
 @app.route('/add_stock',methods=['GET','POST'])
 def add_stock():
